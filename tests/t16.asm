51bf9973fdbb1a2f68f657ed044bbe2d5d3a2da3
Michael Rasmussen <michaelrasmussen1337@gmail.com>
2019-07-12 19:08:25 +0200

    Fix bug in RetireLocals and InstROT to match C version output

--- tests/t16.asm
+++ tests/t16.asm
@@ -2,13 +2,20 @@
 Start:
         stc
         clc
+        rol dx, 12
+        shr si, 4
         mov ax, .Foo
 .Foo:
+.Foo2:
 Bar:
+.Foo2:
 .Foo:
 Baz:
+.Foo3:
 .Foo:
+.Foo2:
 Foo:
+.Fzz:
 .Foo:
 Fuz:
         mov ax, .Foo
